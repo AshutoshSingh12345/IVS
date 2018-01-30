943b96149955fae9ed40c0c5158d99627180e777
Ashutosh Singh <ashushubdata@gmail.com>
2018-01-30 19:54:03 +0530

    Add files via upload

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,74 @@
+     <aside id="slide-out" class="side-nav red darken-4 fixed">
+                <div class="side-nav-wrapper">
+                    <div class="sidebar-profile">
+                        <div class="sidebar-profile-image">
+                            <img src="../assets/images/profile-image.png" class="circle" alt="">
+                        </div>
+                        <div class="sidebar-profile-info">
+                       
+                                <p>Admin</p>
+
+                         
+                        </div>
+                    </div>
+            
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+					<li class="no-padding"><a class="waves-effect waves-grey" href="../IVS_RTL_RAIL/index.php"><i class="material-icons">home</i>Home</a></li>
+                    <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php"><i class="material-icons">dashboard</i>Dashboard</a></li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">apps</i>Account<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addaccount.php">Add Account</a></li>
+                                <li><a href="manageaccounts.php">Manage Account</a></li>
+                            </ul> 
+                        </div>
+                    </li>
+					<li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">assessment</i>Track<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addtrack.php">Add Track</a></li>
+                                <li><a href="managetrack.php">Manage Track</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_box</i>Employees<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addemployee.php">Add Employee</a></li>
+                                <li><a href="manageemployee.php">Manage Employee</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+   <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">confirmation_number</i>Leave Tracker<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="leaves.php">All Leaves </a></li>
+                                <li><a href="pending-leavehistory.php">Pending Leaves </a></li>
+                                <li><a href="approvedleave-history.php">Approved Leaves</a></li>
+                                  <li><a href="notapproved-leaves.php">Not Approved Leaves</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+<li class="no-padding"><a class="waves-effect waves-grey" href="ipt.php"><i class="material-icons">access_alarm</i>IPT</a></li>
+<li class="no-padding"><a class="waves-effect waves-grey" href="changepassword.php"><i class="material-icons">crop</i>Change Password</a></li>
+
+                        <li class="no-padding">
+                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Sign Out</a>
+                            </li>  
+                 
+
+                 
+              
+                </ul>
+                   
+                </div>
+            </aside>
